6afabb2f08f09cb00562219ea95b73d10cc164b2
Shikhar <shikharsharma1408@gamil.com>
2023-06-13 17:55:10 +0530

    full Commit

---
+++ lib/providers/cart.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final int id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.title,
+    required this.quantity,
+    required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<int, CartItem> _items = {};
+
+  Map<int, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+
+  void addItem(
+      int productId,
+    double price,
+    String title,
+  ) {
+    if (_items.containsKey(productId)) {
+      // change quantity...
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+              id: existingCartItem.id,
+              title: existingCartItem.title,
+              price: existingCartItem.price,
+              quantity: existingCartItem.quantity + 1,
+            ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+              id: DateTime.now() as int,
+              title: title,
+              price: price,
+              quantity: 1,
+            ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removeItem(int productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void removeSingleItem(int  productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId]!.quantity > 1) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+                id: existingCartItem.id,
+                title: existingCartItem.title,
+                price: existingCartItem.price,
+                quantity: existingCartItem.quantity - 1,
+              ));
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
+
+  void clear() {
+    _items = {};
+    notifyListeners();
+  }
+}
